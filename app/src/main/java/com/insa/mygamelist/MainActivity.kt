d377cba321576afa002581959d5345a22590cbf9
Siyi.Chen <siyi.chen@insa-rennes.fr>
2025-01-31 16:19:46 +0100

    list game

--- app/src/main/java/com/insa/mygamelist/MainActivity.kt
+++ app/src/main/java/com/insa/mygamelist/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.insa.mygamelist
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -8,13 +9,17 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.insa.mygamelist.data.GameItem
 import com.insa.mygamelist.data.IGDB
 
@@ -35,9 +40,9 @@ class MainActivity : ComponentActivity() {
                         titleContentColor = Color.Black,
                     ), title = { Text("My Games List") })
                 }, modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column(modifier = Modifier.padding(innerPadding)) {
+                    LazyColumn(modifier = Modifier.padding(innerPadding)) {
 
-                        IGDB.games.map{ game->GameItem(game) }
+                        items(IGDB.games){ game -> GameItem(game)}
 
                     }
                 }
--- app/src/main/java/com/insa/mygamelist/data/GameItem.kt
+++ app/src/main/java/com/insa/mygamelist/data/GameItem.kt
@@ -2,55 +2,73 @@ package com.insa.mygamelist.data
 
 
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
 
 
 @Composable
 fun GameItem(game: Game) {
-    Row(
+    Box(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(8.dp)
+            .padding(14.dp)
+            .background(Color.LightGray, shape = RoundedCornerShape(8.dp))
     ) {
-        AsyncImage(
-            model = "https:"+getCoverUrl(game.cover),
-            contentDescription = "Game Cover",
+        Row(
             modifier = Modifier
-                .size(100.dp)
+                .fillMaxWidth()
                 .padding(8.dp)
-        )
-
-        Column(
-            modifier = Modifier
-                .padding(start = 8.dp)
-                .align(Alignment.CenterVertically)
         ) {
-
-            Text(
-                text = game.name,
-                style = MaterialTheme.typography.titleMedium,
-                color = Color.Black
+            AsyncImage(
+                model = "https:" + getCoverUrl(game.cover),
+                contentDescription = "Game Cover",
+                modifier = Modifier
+                    .size(100.dp)
+                    .padding(8.dp)
             )
 
-            val genreNames = game.genres.mapNotNull { id -> IGDB.genres.find { it.id == id }?.name }
-            Text(
-                text = "Genres: ${genreNames.joinToString(", ")}",
-                style = MaterialTheme.typography.bodySmall
-            )
+            Column(
+                modifier = Modifier
+                    .padding(start = 4.dp)
+                    .align(Alignment.CenterVertically)
+            ) {
 
+                Text(
+                    text = game.name,
+                    style = TextStyle(fontWeight = FontWeight.Bold,  textDecoration = TextDecoration.Underline),
+                    color = Color.Black
+
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+                val genreNames =
+                    game.genres.mapNotNull { id -> IGDB.genres.find { it.id == id }?.name }
+                Text(
+                    text = "Genres: ${genreNames.joinToString(", ")}",
+                    style = MaterialTheme.typography.bodySmall,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+
+
+            }
 
         }
-
     }
 }
+
 
 
 fun getCoverUrl(coverId: Long): String {
